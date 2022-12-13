9743702c5994bfc9a9de36166f8f0a2510fcd204
dhrumilbhut <dhrumilbhut@gmail.com>
2022-12-13 22:30:40 +0530

    created a mail utility

---
+++ utils/mailHelper.js
@@ -0,0 +1,16 @@
+import config from "../config/index";
+import transporter from "../config/transporter.config";
+
+const mailHelper = async (options) => {
+  const message = {
+    from: config.SMTP_MAIL_EMAIL, // sender address
+    to: options.email, // list of receivers
+    subject: options.subject, // Subject line
+    text: options.text, // plain text body
+    // html: "<b>Hello world?</b>", // html body
+  };
+
+  await transporter.sendMail(message);
+};
+
+export default mailHelper;
