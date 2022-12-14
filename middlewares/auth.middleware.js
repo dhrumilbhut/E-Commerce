dd4350137fc499b51bd4fae1da398e5be21aba6c
dhrumilbhut <dhrumilbhut@gmail.com>
2022-12-14 22:24:23 +0530

    Added middleware for login

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+import User from "../models/user.schema";
+import JWT from "jsonwebtoken";
+import asyncHandler from "../services/asyncHandler";
+import customError from "../utils/customError";
+import config from "../config/index";
+
+export const isLoggedIn = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.cookies.token ||
+    (req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer"))
+  ) {
+    token = req.cookies.token || req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    throw new customError("Not authorized to access this route", 401);
+  }
+
+  try {
+    const decodedJwtPayload = JWT.verify(token, config.JWT_SECRET);
+
+    req.user = await User.findById(decodedJwtPayload._id, "name email role");
+    next();
+  } catch (error) {
+    throw new customError("Not authorized to access this route", 401);
+  }
+});
